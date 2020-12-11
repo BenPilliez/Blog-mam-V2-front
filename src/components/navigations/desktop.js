bac91e1f212180d89631947837a87eb75e87cbfb
Benjamin Pilliez <benjamin.pilliez@gmail.com>
2020-12-11 17:14:55 +0100

    presque bon menu desktop

--- src/components/navigations/desktop.js
+++ src/components/navigations/desktop.js
@@ -1,7 +1,20 @@
 import React from "react"
-import {AppBar, makeStyles, Tab, Tabs, Toolbar, Typography} from "@material-ui/core"
+import {
+    AppBar,
+    Avatar,
+    IconButton,
+    makeStyles,
+    Menu,
+    MenuItem,
+    Tab,
+    Tabs,
+    Toolbar,
+    Tooltip,
+    Typography
+} from "@material-ui/core"
 import {Link as RouterLink} from "react-router-dom"
 import ThemeSwitch from "./themeSwitch"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,18 +27,45 @@ const useStyles = makeStyles((theme) => ({
     },
     themeSwitch: {
         display: 'flex',
-        justifyContent: 'flex-end'
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     }
 }))
 
-const DesktopNav = ({categories, isDark, handleChange}) => {
+const DesktopNav = ({categories, isDark, handleChange, user, handleLogout, handleOpen}) => {
 
     const [value, setValue] = React.useState('home')
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const [files] = React.useState(user ? user.avatar : '')
+    const isMenuOpen = Boolean(anchorEl)
     const classes = useStyles()
 
     const handleChangeValue = (event, newValue) => {
         setValue(newValue)
     }
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleMenuClose = () => {
+        setAnchorEl(null)
+    }
+
+    const userMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Mon compte</MenuItem>
+            <MenuItem onClick={handleLogout}>Se déconnecter</MenuItem>
+        </Menu>
+    )
+
 
     return (
         <AppBar className={classes.root} color={isDark ? 'primary' : 'transparent'} elevation={0}>
@@ -53,6 +93,16 @@ const DesktopNav = ({categories, isDark, handleChange}) => {
                     })}
                 </Tabs>
                 <div className={classes.themeSwitch}>
+                    <Typography component={"div"}>
+                        <Tooltip title={user ? 'Se déconnecter' : 'Se connecter'}>
+                            {user ? (<React.Fragment>
+                                <Avatar src={`${process.env.REACT_APP_BASE_PUBLIC_URL}/${files}`} onClick={(e) => handleProfileMenuOpen(e)}/>
+                                {userMenu}
+                            </React.Fragment>) : <IconButton onClick={() => handleOpen()}>
+                                <FontAwesomeIcon size={"lg"} icon={'sign-in-alt'}/>
+                            </IconButton>}
+                        </Tooltip>
+                    </Typography>
                     <ThemeSwitch handleChange={handleChange} isDark={isDark}/>
                 </div>
             </Toolbar>
